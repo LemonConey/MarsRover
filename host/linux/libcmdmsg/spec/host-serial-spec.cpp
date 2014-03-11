c97de5b425f6c1293ce2253d63f5e853dcd50155
Coney Wu <coney.wu@gmail.com>
2014-03-11 16:33:43 +0800

    remove spec to libcmdmsg folder

---
+++ host/linux/libcmdmsg/spec/host-serial-spec.cpp
@@ -0,0 +1,6 @@
+#include "spec_helper.h"
+#include "host-serial.h"
+
+TEST(Stream, ShouldReceiveCRLFOnPrintLn)  {
+    ASSERT_TRUE(1);
+}
---
+++ host/linux/libcmdmsg/spec/spec_helper.h
@@ -0,0 +1,7 @@
+#ifndef __SPEC_HELPER_H__
+#define __SPEC_HELPER_H__
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#endif // __SPEC_HELPER_H__
