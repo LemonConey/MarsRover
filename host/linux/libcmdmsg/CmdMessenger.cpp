8a2674a5495eb489956183fd6e49ec4969542224
Coney Wu <coney.wu@gmail.com>
2014-03-16 18:26:06 +0800

    remove CmdMessenger

--- host/linux/libcmdmsg/CmdMessenger.cpp
+++
@@ -1,1 +0,0 @@
-../../../arduino/lib/CmdMessenger.cpp
--- host/linux/libcmdmsg/spec/spec_helper.h
+++
@@ -1,8 +0,0 @@
-#ifndef __SPEC_HELPER_H__
-#define __SPEC_HELPER_H__
-
-#include "gtest/gtest.h"
-#include "gmock/gmock.h"
-using namespace testing;
-
-#endif // __SPEC_HELPER_H__
