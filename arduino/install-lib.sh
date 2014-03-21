baf311e4031a368b4940f56dc779196aaf627f69
Coney Wu <coney.wu@gmail.com>
2014-03-21 18:51:10 +0800

    add script to install rover lib to arduino root

---
+++ arduino/install-lib.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Usage:
+# install_lib.sh src-lib dst-lib
+
+#export ARDUINO_ROOT=/Users/coney/Applications/Arduino.app/Contents/Resources/Java
+ARDUINO_ROOT=${1:?no arduino root specified}
+ARDUINO_LIB=$ARDUINO_ROOT/libraries
+
+function install {
+  TARGET=$ARDUINO_LIB/$2
+  echo install $1 to $TARGET
+  if ! [[ -e $TARGET ]]; then
+    ln -sf `pwd`/$1 $ARDUINO_LIB/$2
+  fi
+}
+
+#echo will install libraries to $ARDUINO_LIB
+
+install lib rover-lib
+install nanopb nanopb
+install protocol rover-proto
