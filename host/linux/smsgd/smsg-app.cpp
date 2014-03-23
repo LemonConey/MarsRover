40b2b702ec6ec70d022274e9462d900d8bbb2306
Coney Wu <coney.wu@gmail.com>
2014-03-23 18:43:45 +0800

    move main to SMessage::main

--- host/linux/smsgd/smsg-app.cpp
+++ host/linux/smsgd/smsg-app.cpp
@@ -1,21 +1,33 @@
 #include <Poco/FileChannel.h>
+#include <Poco/Thread.h>
+
 #include "smsg-common.h"
 #include "smsg-app.h"
+#include "serial-source.h"
+#include "udp-source.h"
+#include "text-client-mgr.h"
+
+using namespace std;
+using namespace Poco;
 using namespace Poco::Util;
+using namespace Poco::Net;
 
 SMessageApp::SMessageApp()
 {
     m_serial_port = SMSGD_DEFAULT_SERIAL_PORT;
     m_udp_port = SMSGD_DEFAULT_UDP_PORT;
     m_log_file = SMSGD_DEFAULT_LOG_PATH;
+
+    m_udp_socket = NULL;
+    m_serial_msg = NULL;
 }
 
 int SMessageApp::main( const std::vector < std::string > & args )
 {
-    while (1) {
-        logdebug("hehe");
-        sleep(1);
-    }
+    std::for_each(m_threads.begin(), m_threads.end(), [](Thread *thread) {
+        thread->join();
+    });
+
     return EXIT_OK;
 }
 
@@ -33,13 +45,34 @@ void SMessageApp::initialize( Application & self )
     DEFAULT_LOGGER.setLevel(Poco::Message::PRIO_TRACE);
 
     if (config().getBool("application.runAsDaemon", false)) {
-        logdebug("write log to %s\n", m_log_file.c_str());
+        logdebug("write log to %s", m_log_file.c_str());
         DEFAULT_LOGGER.setChannel(getFileChannel(m_log_file));
     }
 
     loginfo("log level is %d", DEFAULT_LOGGER.getLevel());
     loginfo("using serial port %s", m_serial_port.c_str());
     loginfo("using udp port %hu", m_udp_port);
+
+    if (!setupSerial()) {
+        logerror("can't connect to %s", m_serial_port.c_str());
+        exit(EXIT_IOERR);
+    }
+
+    if (!setupUdpConn()) {
+        logerror("can't create udp socket bind on %hu", m_udp_port);
+        exit(EXIT_IOERR);
+    }
+
+    if (!createUdpThread()) {
+        logerror("can't create udp thread");
+        exit(EXIT_IOERR);
+    }
+    
+    if (!createSerialThread()) {
+        logerror("can't create serial thread");
+        exit(EXIT_IOERR);
+    }
+    
 }
 
 void SMessageApp::defineOptions( OptionSet & options )
@@ -63,4 +96,112 @@ void SMessageApp::handleOption( const std::string & name, const std::string & va
     else if (name == "log-file") {
         m_log_file = value;
     }
+}
+
+bool SMessageApp::setupSerial()
+{
+    SerialPort *serial_port = new SerialPort(m_serial_port);
+    serial_port->Open(SerialPort::BAUD_9600,
+        SerialPort::CHAR_SIZE_8,
+        SerialPort::PARITY_NONE,
+        SerialPort::STOP_BITS_1,
+        SerialPort::FLOW_CONTROL_NONE);
+
+    if (!serial_port->IsOpen()) {
+        delete serial_port;
+        return false;
+    }
+    
+    m_serial_msg = new SMessage(new SerialSource(serial_port));
+    return true;
+}
+
+bool SMessageApp::setupUdpConn()
+{
+    m_udp_socket = new DatagramSocket();
+    try {
+        m_udp_socket->bind(SocketAddress("0.0.0.0", m_udp_port), true);
+    }
+    catch (Exception &e) {
+        DEFAULT_LOGGER.log(e);
+        return false;
+    }
+    return true;
+}
+
+bool SMessageApp::createSerialThread()
+{
+    Thread *thread = new Thread();
+    thread->start([](void * arg) {
+        SMessageApp *self = (SMessageApp *)arg;
+        loginfo("serial thread started");
+        self->m_serial_msg->onUnhandledMessage([](SMessagePDU::Message *msg, void *) {
+            logtrace("[serial]read msg type:%hhd size:%hhd", 
+                msg->type, msg->size);
+            try {
+                TextClientManager::getInstance()->send(msg->type, 
+                    (const char*)msg, msg->size + sizeof(*msg));
+            }
+            catch (std::exception &e) {
+                logerror("send error:%s", e.what());
+            }
+
+        });
+
+        while (1) {
+            self->m_serial_msg->feed();
+        }
+    }, this);
+
+    m_threads.push_back(thread);
+
+    return true;
+}
+
+bool SMessageApp::createUdpThread()
+{
+    Thread *thread = new Thread();
+    thread->start([](void * arg) {
+        SMessageApp *self = (SMessageApp *)arg;
+        loginfo("udp thread started");
+        while (1) {
+            char buffer[256];
+            SocketAddress address;
+            int ret = self->m_udp_socket->receiveFrom(buffer, sizeof(buffer) - 1, address);
+            if (ret > 0) {
+                buffer[ret] = 0;
+                if (TextClientManager::getInstance()->processCommand(buffer, address)) {
+                    logtrace("handled [%s]", buffer);
+                    continue;
+                }
+
+                SMessagePDU::Message *msg = (SMessagePDU::Message *)buffer;
+                if ((size_t)ret != msg->size + sizeof(*msg)) {
+                    logtrace("[udp] invalid msg type:%hhu size:%hhu ret:%d",
+                        msg->type, msg->size, ret);
+                }
+                else {
+                    logtrace("[udp] read msg type:%hhu size:%hhu",
+                        msg->type, msg->size);
+                    self->m_serial_msg->send(msg->type, msg->data, msg->size);
+                }
+
+            }
+
+        }
+    }, this);
+    m_threads.push_back(thread);
+    return true;
+}
+
+int main(int argc, char **argv) {
+
+    try {
+        throw exception();
+    }
+    catch (std::exception &e) {
+        logdebug("hehe");
+    }
+
+    return SMessageApp().run(argc, argv);
 }
--- host/linux/smsgd/smsg-app.h
+++ host/linux/smsgd/smsg-app.h
@@ -4,6 +4,10 @@
 #include <stdint.h>
 #include <string>
 #include <Poco/Util/ServerApplication.h>
+#include <Poco/Net/DatagramSocket.h>
+#include <SerialPort.h>
+#include <list>
+#include "smessage.h"
 
 class SMessageApp : public Poco::Util::ServerApplication {
 public:
@@ -16,10 +20,20 @@ protected:
         const std::string & value);
 
 private:
+    bool setupSerial();
+    bool setupUdpConn();
+    bool createSerialThread();
+    bool createUdpThread();
+
     Poco::Logger::Channel * getFileChannel(const std::string &path);
-    std::string     m_serial_port;
-    std::string     m_log_file;
-    uint16_t        m_udp_port;
+    std::string                 m_serial_port;
+    std::string                 m_log_file;
+    uint16_t                    m_udp_port;
+
+    Poco::Net::DatagramSocket   *m_udp_socket;
+    SMessage                    *m_serial_msg;
+
+    std::list<Poco::Thread *>        m_threads;
 };
 
 #endif // SMSG_APP_H__
