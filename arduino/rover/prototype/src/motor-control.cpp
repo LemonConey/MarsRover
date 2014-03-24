8e8dd5e7e5841f29805e2bea8bf68ef1ddef89f2
Coney Wu <coney.wu@gmail.com>
2014-03-25 00:00:37 +0800

    using new protocol for rover movement

--- arduino/rover/prototype/src/motor-control.cpp
+++ arduino/rover/prototype/src/motor-control.cpp
@@ -9,10 +9,7 @@ static int inpin[] = {
     MOTOR_L298N_IN4
 };
 
-struct motor_control {
-    unsigned long expires;
-};
-static motor_control motors[2];
+static unsigned long motor_expires = 0;
 
 static void setup_l298n() {
     for (int i = 0; i < ARRAY_SIZE(inpin); ++i)
@@ -45,21 +42,29 @@ static void movement_callback(SMessagePDU::Message *msg, void *) {
     pb_istream_t istream = pb_istream_from_buffer((uint8_t*)msg->data, msg->size);
     pb_decode(&istream, Movement_fields, &mm);
 
-    get_lcd1602()->printAt(1, "duration %d", mm.duration);
+    get_lcd1602()->printAt(0, "%ld:%ld:%ld", 
+        mm.power, mm.direction, mm.duration);
 
-    //get_lcd1602()->printAt(1, "%04d %04d       ", 
-    //    mm->motors[0].power,
-    //    mm->motors[1].power);
+    int left, right;
+    left = right = mm.power;
+    if (mm.direction < 0) {
+        int dir = min(abs(mm.direction), 100);
+        left = left * (100 - dir) / 100;
+    } else if (mm.direction > 0) {
+        int dir = min(abs(mm.direction), 100);
+        right = right * (100 - dir) / 100;
+    }
 
-    //for (size_t i = 0; i < ARRAY_SIZE(motors); ++i) {
-    //    motors[i].expires = millis() + mm->motors[i].duration;
-    //    update_l298n(i, mm->motors[i].power);
-    //}
+    motor_expires = millis() + mm.duration;
+
+    get_lcd1602()->printAt(1, "%d:%d", left, right);
+
+    update_l298n(0, left);
+    update_l298n(1, right);
 }
 
 
 void setup_motor_control() {
-    memset(motors, 0, sizeof(motors));
     setup_l298n();
     get_smessage()->onMessage(Movement_Message_Id, movement_callback);
 }
@@ -67,11 +72,10 @@ void setup_motor_control() {
 
 void update_motor_state()
 {
-    for (size_t i = 0; i < ARRAY_SIZE(motors); ++i) {
-        if (motors[i].expires && millis() >= motors[i].expires) {
-            update_l298n(i, 0);
-            get_lcd1602()->printAt(1, "stop all motors");
-            motors[i].expires = 0;
-        }
+    if (motor_expires && millis() >= motor_expires) {
+        update_l298n(0, 0);
+        update_l298n(1, 0);
+        get_lcd1602()->printAt(1, "stop all motors");
+        motor_expires = 0;
     }
 }
